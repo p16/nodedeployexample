8f0b21cd28406777b775f2b478c1c6b9a1c6bf5e
Filippo De Santis <filippo.desantis@gmail.com>
2016-06-01 23:35:54 +0400

    0 downtime!!!!111!!!!

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World... ing is very smart");
+  response.end("Hello World... 0 downtime!!!");
 });
 server.listen(80);
 console.log("Server running at http://127.0.0.1:8090/");
